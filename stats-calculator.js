e7ab4bd858a9dcbf86369d65ed91b818ac96ede0
AirenMarie <astringer92@gmail.com>
2024-07-17 19:56:08 -0400

    Add getMode() to calculate, change text content for #mode element

--- stats-calculator.js
+++ stats-calculator.js
@@ -41,7 +41,10 @@ const calculate = () => {
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
 
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 };
