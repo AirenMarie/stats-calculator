55f38e312c2e9e47284a94222ba655ce32ccd8e9
AirenMarie <astringer92@gmail.com>
2024-07-17 20:20:58 -0400

    Create 'differences' and 'squaredDifferences' variables in getVariance

--- stats-calculator.js
+++ stats-calculator.js
@@ -41,6 +41,8 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
+  const differences = array.map((el) => el - mean);
+  const squaredDifferences = differences.map((el) => el ** 2);
 };
 
 const calculate = () => {
